3105b85780d415ccb14e5a56e6794d1326864b53
Eric Ferreira <ferreira14eric@gmail.com>
2025-03-18 14:50:13 -0300

    Initial Commit

---
+++ goal-mate-project/goal_mate_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  goal_mate_projectApp.swift
+//  goal-mate-project
+//
+//  Created by User on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct goal_mate_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
